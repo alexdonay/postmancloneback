4a80e82876d3cd49fe445705d27fdd6bcd1697d6
Alexsander <alexdonay@gmail.com>
2025-08-01 22:52:58 -0300

    alterando auth controller

--- src/modules/infra/auth/auth.module.ts
+++ src/modules/infra/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport'; // 1. IMPORTAR O PASSPORTMODULE
 import { JwtStrategy } from './jwt.strategy';
 import { UserModule } from 'src/modules/user/user.module';
 import { AuthService } from './auth.service';
@@ -10,16 +11,20 @@ import { AuthController } from './auth.controller';
   imports: [
     ConfigModule,
     UserModule,
+    // 2. REGISTRAR O PASSPORTMODULE AQUI
+    PassportModule.register({ defaultStrategy: 'jwt' }), 
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60m' },
+        signOptions: { expiresIn: '1d' }, // Aumentei para 1 dia, 60m é pouco para testes
       }),
       inject: [ConfigService],
     }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  // 3. EXPORTAR O PASSPORTMODULE PARA OUTROS MÓDULOS USAREM
+  exports: [PassportModule, JwtStrategy], 
 })
 export class AuthModule {}
